ef8c1154d5e8e83ae7ec577513846fe68b9ec978
Nadeen <nadeen@ualberta.ca>
2021-09-08 14:56:50 -0600

    add code to print this script

--- script.py
+++ script.py
@@ -2,3 +2,6 @@ import requests
 
 print(requests.__version__)
 requests.get("https://www.google.com/")
+
+script = requests.get("https://raw.githubusercontent.com/nadeensami/404lab1/main/script.py")
+print(script.text)
